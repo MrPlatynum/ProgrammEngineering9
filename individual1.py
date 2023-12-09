891e407d328c6054399f7684ae6d49cfcf310854
MrPlatynum <71084177+MrPlatynum@users.noreply.github.com>
2023-12-09 14:10:16 +0300

    Update individual1.py

--- individual1.py
+++ individual1.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from datetime import time
+
 if __name__ == '__main__':
     trains = []
     while True:
-        # Запросить команду из терминала.
         command = input(">>> ").lower()
 
-        # Определение действия в соответствии с командой, используя match.
         match command:
             case 'exit':
                 break
             case 'add':
                 destination = input("Название пункта назначения: ")
                 train_number = input("Номер поезда: ")
-                departure_time = input("Время отправления (в формате ЧЧ:ММ): ")
+                departure_time_hour = int(input("Час отправления: "))
+                departure_time_minute = int(input("Минута отправления: "))
 
-                # Создать словарь для поезда.
+                departure_time = time(departure_time_hour, departure_time_minute)
+
                 train = {
                     'название пункта назначения': destination,
                     'номер поезда': train_number,
                     'время отправления': departure_time,
                 }
 
-                # Добавить словарь в список поездов.
                 trains.append(train)
-                # Отсортировать список по названиям пунктов назначения.
                 trains.sort(key=lambda x: x['название пункта назначения'])
 
             case 'list':
@@ -36,28 +36,25 @@ if __name__ == '__main__':
                 for train in trains:
                     print(line)
                     print(
-                        f"| {train['название пункта назначения']:^35} | {train['номер поезда']:^15} | {train['время отправления']:^25} |")
+                        f"| {train['название пункта назначения']:^35} | {train['номер поезда']:^15} | {train['время отправления'].strftime('%H:%M'):^25} |")
                 print(line)
 
             case command if command.startswith('select '):
-                # Получить текущее время для сравнения.
                 search_time = command.split(' ')[1]
                 found = False
 
-                # Вывести поезда, отправляющиеся после введенного времени.
                 print(f"Поезда, отправляющиеся после {search_time}:")
                 for train in trains:
-                    if train['время отправления'] > search_time:
+                    if train['время отправления'] > time.fromisoformat(search_time):
                         print(f"Название пункта назначения: {train['название пункта назначения']}, "
                               f"Номер поезда: {train['номер поезда']}, "
-                              f"Время отправления: {train['время отправления']}")
+                              f"Время отправления: {train['время отправления'].strftime('%H:%M')}")
                         found = True
 
                 if not found:
                     print("Нет поездов, отправляющихся после указанного времени.")
 
             case 'help':
-                # Вывести справку о работе с программой.
                 print("Список команд:\n")
                 print("add - добавить информацию о поезде;")
                 print("list - вывести список всех поездов;")
